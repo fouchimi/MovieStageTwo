e399d8af3fa607772e4afd4d6ab72d8a7926e1e4
Ousmane <ousmanefouchimi1990@gmail.com>
2016-08-21 17:41:11 -0600

    Adding classes and packages

---
+++ Movies2/app/src/main/java/com/example/ousmane/movies/network/HttpManager.java
@@ -0,0 +1,77 @@
+package com.example.ousmane.movies.network;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by ousmane on 8/17/16.
+ */
+public class HttpManager {
+    private static final String LOG_TAG = HttpManager.class.getSimpleName();
+
+    public String getData(String baseUrl) {
+        String data = "";
+        HttpURLConnection httpURLConnection = null;
+
+        try {
+            Uri buildUri = Uri.parse(baseUrl).buildUpon()
+                    .build();
+
+            URL url = new URL(buildUri.toString());
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.connect();
+
+            int status = httpURLConnection.getResponseCode();
+            
+            InputStream in = httpURLConnection.getInputStream();
+
+            data = readStream(in);
+
+        }catch (MalformedURLException e) {
+
+        }catch(IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(httpURLConnection != null) {
+                httpURLConnection.disconnect();
+            }
+        }
+        return data;
+    }
+
+    private String readStream(InputStream in) {
+
+        BufferedReader reader = null;
+        StringBuffer data  = new StringBuffer();
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while((line = reader.readLine()) != null) {
+                data.append(line + "\n");
+            }
+
+        }catch(IOException e) {
+            Log.v(LOG_TAG, "IOException");
+        }finally {
+            if(reader != null) {
+                try {
+                    reader.close();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return data.toString();
+    }
+
+}
---
+++ Movies2/app/src/main/java/com/example/ousmane/movies/parsers/MovieJSONParser.java
@@ -0,0 +1,40 @@
+package com.example.ousmane.movies.parsers;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ousmane on 8/17/16.
+ */
+public class MovieJSONParser {
+    private Movie movie;
+    private List<Movie> list = new ArrayList<>();
+    private static final String THUMBNAIL_BASE_URL = "http://image.tmdb.org/t/p/";
+    private static final String THUMBNAIL_SIZE = "w780/";
+
+    public List<Movie> parseData(String json) {
+        try {
+            JSONObject rootObject = new JSONObject(json);
+            JSONArray results = rootObject.getJSONArray("results");
+            for(int i=0; i < results.length(); i++) {
+                JSONObject eachObject = results.getJSONObject(i);
+                movie = new Movie();
+                movie.setMovieId(eachObject.getString("id"));
+                movie.setTitle(eachObject.getString("original_title"));
+                movie.setDate(eachObject.getString("release_date"));
+                movie.setImage(THUMBNAIL_BASE_URL + THUMBNAIL_SIZE + eachObject.getString("poster_path"));
+                movie.setSynopsis(eachObject.getString("overview"));
+                movie.setRating(eachObject.getDouble("vote_average"));
+                list.add(movie);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
