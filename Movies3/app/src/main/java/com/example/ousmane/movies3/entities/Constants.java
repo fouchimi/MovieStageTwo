d585ce3b206e5969694bf1702211ffa77d1f3a41
fousmane <ousmanefouchimi1990@gmail.com>
2016-08-25 19:27:06 -0500

    Removed my API key

--- Movies3/app/src/main/java/com/example/ousmane/movies3/entities/Constants.java
+++ Movies3/app/src/main/java/com/example/ousmane/movies3/entities/Constants.java
@@ -4,7 +4,7 @@ package com.example.ousmane.movies3.entities;
  * Created by ousmane on 8/17/16.
  */
 public enum Constants {
-    API_KEY("c76c51a7ca44eb1182055d5eb5a576e8"),
+    API_KEY("API_KEY_GOES_HERE"),
     MOVIE_KEY("MOVIE_KEY"),
     FAVORITE_ITEMS("FAVORITE_ITEMS"),
     LARGE("LARGE");
